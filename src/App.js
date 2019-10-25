bad3c38c0522eddc943dd68e4c325cc0a12f7cbb
Rafa <rafaella_souza@gap.com>
2019-10-25 13:22:34 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, useState, useEffect, Fragment} from 'react';
+import Lindo from "./Lindo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const City = () => {
+    const [city, setCity] = useState("Recife");
+
+    const [num, setNum] = useState(100);
+    const [countries, setCountries] = useState([]);
+    
+    useEffect(() => {
+        setCountries(countries);
+    }, [])
+
+    const handleCity = (event) => setCity(event.target.value); 
+
+    return (
+        <Fragment>
+        <div style={{border: `1px solid ${city === 'Recife' ? 'pink': 'black'}`}}>
+            Cidade: {city}<br/>
+            <input type="text" onChange={handleCity} />
+            Habitantes: {num}
+            <input type="number" onChange={(event) => setNum(event.target.value)} />
+        </div>
+        <div>
+            OI
+        </div>
+        </Fragment>
+    )
+}
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = { 
+            displayBio: false,
+            country: 'Brasil',
+        };
+
+    }
+
+    async componentDidMount() {
+    }
+
+    handleCountry = (event) => {
+        const country = event.target.value;
+        this.setState({
+            country,
+        });
+    };
+
+
+    render() {
+        return (
+            <div>
+                <h1>Hello! {this.state.country}</h1>
+                <City />
+                <Lindo name="Rafa" age={20} />
+                <Lindo name="Murilo" />
+                Pais: <input type="text" onChange={this.handleCountry} />
+                <p>My name is Rafa. I'm a React engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Recife, and I code every day.</p>
+                            <p>My favorite language is C#, and I think React is awesome</p>
+                            <p>Besides coding, I also love music and pizza!</p>
+                        </div>
+                    ) : null
+                }
+            </div>
+        )
+    }
+
 }
 
 export default App;
---
+++ src/Lindo.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const  Lindo = ({name, age}) => 
+    <p>Lindo (a): {name} {age && `idade: ${age}`}</p>
+
+export default Lindo;
