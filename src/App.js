f4b7c757ba8e18f28d654d29ee044629d6251011
Rafaella Keury <rafaella.keury@gmail.com>
2019-11-23 21:04:47 -0300

    Add style and use props

--- src/App.js
+++ src/App.js
@@ -1,64 +1,20 @@
-import React, {Component, useState, useEffect, Fragment} from 'react';
-import Lindo from "./Lindo";
-
-
-const City = () => {
-    const [city, setCity] = useState("Recife");
-
-    const [num, setNum] = useState(100);
-    const [countries, setCountries] = useState([]);
-    
-    useEffect(() => {
-        setCountries(countries);
-    }, [])
-
-    const handleCity = (event) => setCity(event.target.value); 
-
-    return (
-        <Fragment>
-        <div style={{border: `1px solid ${city === 'Recife' ? 'pink': 'black'}`}}>
-            Cidade: {city}<br/>
-            <input type="text" onChange={handleCity} />
-            Habitantes: {num}
-            <input type="number" onChange={(event) => setNum(event.target.value)} />
-        </div>
-        <div>
-            OI
-        </div>
-        </Fragment>
-    )
-}
-
+import React, {Component} from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png'
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = { 
-            displayBio: false,
-            country: 'Brasil',
-        };
+    state = {displayBio: false};
 
+    toggleDisplayBio =  () => {
+        this.setState({displayBio: !this.state.displayBio});
     }
-
-    async componentDidMount() {
-    }
-
-    handleCountry = (event) => {
-        const country = event.target.value;
-        this.setState({
-            country,
-        });
-    };
-
 
     render() {
         return (
             <div>
+                <img src={profile} alt='profile' className='profile'/>
                 <h1>Hello! {this.state.country}</h1>
-                <City />
-                <Lindo name="Rafa" age={20} />
-                <Lindo name="Murilo" />
-                Pais: <input type="text" onChange={this.handleCountry} />
                 <p>My name is Rafa. I'm a React engineer.</p>
                 <p>I'm always looking forward to working on meaningful projects.</p>
                 {
@@ -67,13 +23,21 @@ class App extends Component {
                             <p>I live in Recife, and I code every day.</p>
                             <p>My favorite language is C#, and I think React is awesome</p>
                             <p>Besides coding, I also love music and pizza!</p>
+                            <button onClick={this.toggleDisplayBio}>Show less</button>
                         </div>
-                    ) : null
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )
                 }
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
             </div>
         )
     }
-
 }
 
 export default App;
---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'mailto:rafaella.keury@gmail.com',
+        image: emailIcon
+    },
+    {
+        id: 2,
+        link: 'https://github.com/rafaellakeury',
+        image: githubIcon
+    },
+    {
+        id: 3,
+        link: 'https://www.linkedin.com/in/rafaella-keury-a5233123/',
+        image: linkedinIcon
+    },
+    {
+        id: 4,
+        link: 'https://twitter.com/RafaellaKeury',
+        image: twitterIcon
+    },
+];
+
+export default SOCIAL_PROFILES;
